283d26a04c32c7921f942dbf587a6ec63708cee1
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-08-13 19:58:20 -0300

    [add] condicionais basicas e input/parse

---
+++ condicionais/Program.cs
@@ -0,0 +1,18 @@
+﻿const int IDADE_MAX = 99;
+
+Console.Write("Insira sua idade: ");
+
+// é necessário parsear a string pra um int pois o readline apenas lê strings
+int idade = Convert.ToInt32(Console.ReadLine()); 
+
+if (idade < 0 || idade > IDADE_MAX)
+{
+    Console.WriteLine("Idade inválida");
+}
+else if (idade >= 18)
+{
+    Console.WriteLine("Você é maior de idade");
+}else if (idade < 18)
+{
+    Console.WriteLine("Você é MENOR de idade!");
+}
