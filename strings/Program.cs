e019949ea10b770a45e06b399644fa863e714cc8
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-08-13 14:27:33 -0300

    [add] manipulação de Strings

---
+++ strings/Program.cs
@@ -0,0 +1,12 @@
+﻿string nome1 = "    interesse";
+string nome2 = "carros  ";
+string frase = $"Eu tenho {nome1.Trim()} e gosto de {nome2.Trim()}";
+
+Console.WriteLine(frase); // string original
+Console.WriteLine("======================");
+Console.WriteLine(frase.Replace("interesse", "dinheiro")); // string modificada, sem alterar a original
+
+frase = frase.Replace("interesse","dinheiro"); // string origial, agora modificada pra sempre
+
+Console.WriteLine("======================");
+Console.WriteLine(frase); 
