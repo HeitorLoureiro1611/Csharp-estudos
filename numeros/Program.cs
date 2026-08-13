5c9e4a685370b4cc12512e360152471c327c6945
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-08-13 16:42:28 -0300

    [add] tipos numericos

---
+++ numeros/Program.cs
@@ -0,0 +1,12 @@
+﻿// numeros inteiros
+short numero1 = 9999;               // 16 bits
+int   numero2 = 999999999;          // 32 bits
+long  numero3 = 999999999999999999; // 64 bits
+
+// numeros reais
+float   real1 = 1.0F; // sufixo necessário
+double  real2 = 1.0;  // "double precision" 
+decimal real3 = 1.0M; // tipo explicito e preciso
+
+Console.WriteLine($"short: {numero1}\nint: {numero2}\nlong: {numero3}");
+Console.WriteLine($"\nfloat: {real1}\ndouble: {real2}\ndecimal: {real3}");
