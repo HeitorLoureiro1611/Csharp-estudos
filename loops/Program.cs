2041042f57ef0184faf07e65fc1f8f70c775938f
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-08-13 22:47:48 -0300

    [add] sintaxe loops

---
+++ loops/Program.cs
@@ -0,0 +1,22 @@
+﻿int doWhileCont = 0;
+int whileCount = 0;
+
+Console.WriteLine("\nDo While:");
+do
+{
+    doWhileCont++;
+    Console.WriteLine(doWhileCont);
+} while (doWhileCont < 5);
+
+Console.WriteLine("\nWhile:");
+while(whileCount < 10)
+{
+    whileCount++;
+    Console.WriteLine(whileCount);
+}
+
+Console.WriteLine("\nFor:");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(i);
+}
