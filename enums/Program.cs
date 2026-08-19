bd048dafa4e417a00e2e0f12eb97b0c1b01ed9d9
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-08-19 15:40:02 -0300

    [add] enums

---
+++ enums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        struct Item
+        {
+            public Item(double Valor, string? Nome, EProdutoTipo Tipo)
+            {
+                Valor = valor;
+                Nome = nome;
+                Tipo = tipo;
+            }
+            public double valor;
+            public string? nome;
+            public EProdutoTipo tipo; // declaração de um item do tipo do enum
+        }
+
+        enum EProdutoTipo // enum declarado 
+        {
+            Produto = 1,
+            Servico = 2
+        }
+        static void Main(string[] args)
+        {
+            var mouse = new Item(39.99, "Mouse", EProdutoTipo.Produto);
+            var mecanico = new Item(300.00, "revisão de carro", EProdutoTipo.Servico);
+        }
+    }
+}
