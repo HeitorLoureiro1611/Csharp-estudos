e0cc415846a207652d0234d1382813125ea447a4
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-08-19 13:18:30 -0300

    [add] exemplo de listas

---
+++ lists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var arr = new String[2];
+            arr[0] = "primeiro item";
+            arr[1] = "Segundo item";
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            var intarr = new List<int>();
+            intarr.Add(1); 
+            intarr.Add(2);
+            
+            foreach (int n in intarr)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
